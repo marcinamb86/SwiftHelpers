d135c475aafd2e49f3303ca2d2b533451081c82e
Marcin Ambrochowicz <marcinamb86@icloud.com>
2018-09-28 06:54:07 +0200

    Initiate project

---
+++ SwiftHelpers/SwiftHelpers.h
@@ -0,0 +1,19 @@
+//
+//  SwiftHelpers.h
+//  SwiftHelpers
+//
+//  Created by Marcin Ambrochowicz on 28/09/2018.
+//  Copyright © 2018 Marcin Ambrochowicz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftHelpers.
+FOUNDATION_EXPORT double SwiftHelpersVersionNumber;
+
+//! Project version string for SwiftHelpers.
+FOUNDATION_EXPORT const unsigned char SwiftHelpersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftHelpers/PublicHeader.h>
+
+
