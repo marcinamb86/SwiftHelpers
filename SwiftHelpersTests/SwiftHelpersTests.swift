d1ce757ef5c386d9617bf0d21b3d2190e4caa763
Marcin Ambrochowicz <marcinamb86@icloud.com>
2018-09-28 07:21:03 +0200

    Add unit tests

---
+++ SwiftHelpersTests/SwiftHelpersTests.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftHelpersTests.swift
+//  SwiftHelpersTests
+//
+//  Created by Marcin Ambrochowicz on 28/09/2018.
+//  Copyright © 2018 Marcin Ambrochowicz. All rights reserved.
+//
+
+import XCTest
+
+class SwiftHelpersTests : XCTestCase {
+
+    override func setUp() {
+        
+    }
+
+    override func tearDown() {
+        
+    }
+
+}
